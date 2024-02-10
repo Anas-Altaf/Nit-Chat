90dc58dce28ebb46c1c9e5b4a6b351b6ffd71877
Anas Altaf <157444488+Anas-Altaf@users.noreply.github.com>
2024-02-10 18:34:54 +0000

    Improved Performance. Like Added Scroll Bar in Chat Box

--- index.js
+++ index.js
@@ -1,43 +1,45 @@
-const http = require('http');
-const express = require('express');
-const path = require('path'); // Import the path module
-const socketio = require('socket.io');
+const http = require("http");
+const express = require("express");
+const path = require("path"); // Import the path module
+const socketio = require("socket.io");
 
 const app = express();
+
 //Express STuff
-app.use('/static', express.static('./static'));
+app.use("/static", express.static("static"));
 const server = http.createServer(app);
 const io = socketio(server);
 
 const users = {};
 
-io.on('connection', socket => {
-    socket.on('new-user-joined', name => {
-        users[socket.id] = name;
-        socket.broadcast.emit('user-joined', name);
-    });
+io.on("connection", (socket) => {
+  socket.on("new-user-joined", (name) => {
+    users[socket.id] = name;
+    socket.broadcast.emit("user-joined", name);
+  });
 
-    socket.on('send', message => {
-        socket.broadcast.emit('receive', { message: message, name: users[socket.id] });
+  socket.on("send", (message) => {
+    socket.broadcast.emit("receive", {
+      message: message,
+      name: users[socket.id],
     });
-    socket.on('disconnect', message=>{
-        socket.broadcast.emit('left', users[socket.id]);
-        delete users[socket.id];    
-    })
+  });
+  socket.on("disconnect", (message) => {
+    socket.broadcast.emit("left", users[socket.id]);
+    delete users[socket.id];
+  });
 });
-
-
 
 // Define the path to the current directory
 const currentDirPath = __dirname;
 
 // End Points
-app.get('/', (req, res) => {
-    // Serve the index.html file from the current directory
-    res.sendFile('index.html', { root: currentDirPath });
+app.get("/", (req, res) => {
+  // Serve the index.html file from the current directory
+  res.sendFile("index.html", { root: currentDirPath });
 });
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
